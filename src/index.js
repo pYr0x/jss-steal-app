0907aa383c8b73b063a1a0b239bb496fef730135
Julian <julian@MacBook-Pro.local>
2017-07-25 23:46:08 +0200

    jss failed to load

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import "can-view-autorender";
 import Component from "can-component";
 import DefineMap from "can-define/map/";
 
+import jss from 'jss';
+import preset from 'jss-preset-default';
+
 import tmpl from "./index.stache";
+
+jss.setup(preset());
+
+// Create your style.
+const style = {
+	myButton: {
+		color: 'green'
+	}
+};
+
+// Compile styles, apply plugins.
+const sheet = jss.createStyleSheet(style);
+
 
 const HelloWorldVM = DefineMap.extend({
 	message: {value: "Hello There!"}
